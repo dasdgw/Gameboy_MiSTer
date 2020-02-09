21aff0f36b3acc36b3d0bb01210dc9e9b6a8b29d
mazola2k <mazsola2k@modernhackers.com>
2020-02-09 14:37:02 +0100

    Arrow SoCKit

--- sys/sys.tcl
+++ sys/sys.tcl
@@ -1,5 +1,5 @@
 set_global_assignment -name FAMILY "Cyclone V"
-set_global_assignment -name DEVICE 5CSXFC6D6F31C6
+set_global_assignment -name DEVICE 5CSXFC6D6F31C8ES
 #set_global_assignment -name DEVICE_FILTER_PACKAGE UFBGA
 #set_global_assignment -name DEVICE_FILTER_PIN_COUNT 672
 #set_global_assignment -name DEVICE_FILTER_SPEED_GRADE 7
@@ -57,45 +57,45 @@ set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to SDCD_SPDIF
 #============================================================
 # SDRAM
 #============================================================
-set_location_assignment PIN_AK14 -to SDRAM_A[0]
-set_location_assignment PIN_AH14 -to SDRAM_A[1]
-set_location_assignment PIN_AG15 -to SDRAM_A[2]
-set_location_assignment PIN_AE14 -to SDRAM_A[3]
-set_location_assignment PIN_AB15 -to SDRAM_A[4]
-set_location_assignment PIN_AC14 -to SDRAM_A[5]
-set_location_assignment PIN_AD14 -to SDRAM_A[6]
-set_location_assignment PIN_AF15 -to SDRAM_A[7]
-set_location_assignment PIN_AH15 -to SDRAM_A[8]
-set_location_assignment PIN_AG13 -to SDRAM_A[9]
-set_location_assignment PIN_AG12 -to SDRAM_A[10]
-set_location_assignment PIN_AH13 -to SDRAM_A[11]
-set_location_assignment PIN_AJ14 -to SDRAM_A[12]
-set_location_assignment PIN_AF13 -to SDRAM_BA[0]
-set_location_assignment PIN_AJ12 -to SDRAM_BA[1]
-set_location_assignment PIN_AK6 -to SDRAM_DQ[0]
-set_location_assignment PIN_AJ7 -to SDRAM_DQ[1]
-set_location_assignment PIN_AK7 -to SDRAM_DQ[2]
-set_location_assignment PIN_AK8 -to SDRAM_DQ[3]
-set_location_assignment PIN_AK9 -to SDRAM_DQ[4]
-set_location_assignment PIN_AG10 -to SDRAM_DQ[5]
-set_location_assignment PIN_AK11 -to SDRAM_DQ[6]
-set_location_assignment PIN_AJ11 -to SDRAM_DQ[7]
-set_location_assignment PIN_AH10 -to SDRAM_DQ[8]
-set_location_assignment PIN_AJ10 -to SDRAM_DQ[9]
-set_location_assignment PIN_AJ9 -to SDRAM_DQ[10]
-set_location_assignment PIN_AH9 -to SDRAM_DQ[11]
-set_location_assignment PIN_AH8 -to SDRAM_DQ[12]
-set_location_assignment PIN_AH7 -to SDRAM_DQ[13]
-set_location_assignment PIN_AJ6 -to SDRAM_DQ[14]
-set_location_assignment PIN_AJ5 -to SDRAM_DQ[15]
-set_location_assignment PIN_AB13 -to SDRAM_DQML
-set_location_assignment PIN_AK12 -to SDRAM_DQMH
-set_location_assignment PIN_AH12 -to SDRAM_CLK
-set_location_assignment PIN_AK13 -to SDRAM_CKE
-set_location_assignment PIN_AA13 -to SDRAM_nWE
-set_location_assignment PIN_AF11 -to SDRAM_nCAS
-set_location_assignment PIN_AG11 -to SDRAM_nCS
-set_location_assignment PIN_AE13 -to SDRAM_nRAS
+set_location_assignment PIN_B1 -to SDRAM_A[0]
+set_location_assignment PIN_C2 -to SDRAM_A[1]
+set_location_assignment PIN_B2 -to SDRAM_A[2]
+set_location_assignment PIN_D2 -to SDRAM_A[3]
+set_location_assignment PIN_D9 -to SDRAM_A[4]
+set_location_assignment PIN_C7 -to SDRAM_A[5]
+set_location_assignment PIN_E12 -to SDRAM_A[6]
+set_location_assignment PIN_B7 -to SDRAM_A[7]
+set_location_assignment PIN_D12 -to SDRAM_A[8]
+set_location_assignment PIN_A11 -to SDRAM_A[9]
+set_location_assignment PIN_B6 -to SDRAM_A[10]
+set_location_assignment PIN_D11 -to SDRAM_A[11]
+set_location_assignment PIN_A10 -to SDRAM_A[12]
+set_location_assignment PIN_B5 -to SDRAM_BA[0]
+set_location_assignment PIN_A4 -to SDRAM_BA[1]
+set_location_assignment PIN_F14 -to SDRAM_DQ[0]
+set_location_assignment PIN_G15 -to SDRAM_DQ[1]
+set_location_assignment PIN_F15 -to SDRAM_DQ[2]
+set_location_assignment PIN_H15 -to SDRAM_DQ[3]
+set_location_assignment PIN_G13 -to SDRAM_DQ[4]
+set_location_assignment PIN_A13 -to SDRAM_DQ[5]
+set_location_assignment PIN_H14 -to SDRAM_DQ[6]
+set_location_assignment PIN_B13 -to SDRAM_DQ[7]
+set_location_assignment PIN_C13 -to SDRAM_DQ[8]
+set_location_assignment PIN_C8 -to SDRAM_DQ[9]
+set_location_assignment PIN_B12 -to SDRAM_DQ[10]
+set_location_assignment PIN_B8 -to SDRAM_DQ[11]
+set_location_assignment PIN_F13 -to SDRAM_DQ[12]
+set_location_assignment PIN_C12 -to SDRAM_DQ[13]
+set_location_assignment PIN_B11 -to SDRAM_DQ[14]
+set_location_assignment PIN_E13 -to SDRAM_DQ[15]
+set_location_assignment -remove -to SDRAM_DQML
+set_location_assignment -remove -to SDRAM_DQMH
+set_location_assignment PIN_D10 -to SDRAM_CLK
+set_location_assignment -remove -to SDRAM_CKE
+set_location_assignment PIN_A5 -to SDRAM_nWE
+set_location_assignment PIN_A6 -to SDRAM_nCAS
+set_location_assignment PIN_A3 -to SDRAM_nCS
+set_location_assignment PIN_E9 -to SDRAM_nRAS
 
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to SDRAM_*
 set_instance_assignment -name CURRENT_STRENGTH_NEW "MAXIMUM CURRENT" -to SDRAM_*
@@ -103,7 +103,6 @@ set_instance_assignment -name FAST_OUTPUT_REGISTER ON -to SDRAM_*
 set_instance_assignment -name FAST_OUTPUT_ENABLE_REGISTER ON -to SDRAM_DQ[*]
 set_instance_assignment -name FAST_INPUT_REGISTER ON -to SDRAM_DQ[*]
 set_instance_assignment -name ALLOW_SYNCH_CTRL_USAGE OFF -to *|SDRAM_*
-
 
 #============================================================
 # SPI SD
@@ -120,17 +119,45 @@ set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to SD_SPI*
 #============================================================
 # CLOCK
 #============================================================
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK1_50
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK2_50
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK3_50
+#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK1_50
+#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK2_50
+#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK3_50
+#Eredeti
 set_location_assignment PIN_AF14 -to FPGA_CLK1_50
 set_location_assignment PIN_AA16 -to FPGA_CLK2_50
 set_location_assignment PIN_Y26 -to FPGA_CLK3_50
+#Minimig mar jo
+#set_location_assignment PIN_AF14 -to FPGA_CLK1_50
+#set_location_assignment PIN_Y26 -to FPGA_CLK2_50
+#set_location_assignment PIN_AA16 -to FPGA_CLK3_50
+
+#set_location_assignment PIN_AF14 -to FPGA_CLK1_50
+#set_location_assignment PIN_K14 -to FPGA_CLK2_50
+#set_location_assignment PIN_AA16 -to FPGA_CLK3_50
+
+#set_location_assignment PIN_AA16 -to FPGA_CLK1_50
+#set_location_assignment PIN_Y26 -to FPGA_CLK2_50
+#set_location_assignment PIN_K14 -to FPGA_CLK3_50
+
+#meg jo lehet cserelt
+#set_location_assignment PIN_AA16 -to FPGA_CLK1_50
+#set_location_assignment PIN_Y26 -to FPGA_CLK2_50
+#set_location_assignment PIN_AF14 -to FPGA_CLK3_50
+
+#teljesen lecserelt
+
+#set_location_assignment PIN_AA16 -to FPGA_CLK1_50
+#set_location_assignment PIN_AF14 -to FPGA_CLK2_50
+#set_location_assignment PIN_K14 -to FPGA_CLK3_50
+
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK1_50
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK2_50
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK3_50
+
 
 #============================================================
 # HDMI
 #============================================================
-
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_I2C_*
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_I2S
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_LRCLK
@@ -142,42 +169,6 @@ set_instance_assignment -name FAST_OUTPUT_REGISTER ON -to HDMI_TX_DE
 set_instance_assignment -name FAST_OUTPUT_REGISTER ON -to HDMI_TX_HS
 set_instance_assignment -name FAST_OUTPUT_REGISTER ON -to HDMI_TX_VS
 set_instance_assignment -name FAST_OUTPUT_REGISTER ON -to HDMI_TX_CLK
-
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_I2C_SCL
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_I2C_SDA
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_I2S
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_LRCLK
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_MCLK
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_SCLK
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_CLK
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_DE
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[0]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[1]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[2]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[3]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[4]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[5]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[6]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[7]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[8]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[9]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[10]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[11]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[12]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[13]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[14]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[15]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[16]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[17]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[18]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[19]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[20]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[21]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[22]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[23]
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_HS
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_INT
-#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_VS
 #set_location_assignment PIN_U10 -to HDMI_I2C_SCL
 #set_location_assignment PIN_AA4 -to HDMI_I2C_SDA
 #set_location_assignment PIN_T13 -to HDMI_I2S
@@ -219,8 +210,8 @@ set_instance_assignment -name FAST_OUTPUT_REGISTER ON -to HDMI_TX_CLK
 #============================================================
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to KEY[0]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to KEY[1]
-set_location_assignment PIN_Y27 -to KEY[0]
-set_location_assignment PIN_AB28 -to KEY[1]
+set_location_assignment PIN_V25 -to KEY[0]
+set_location_assignment PIN_AC28 -to KEY[1]
 
 #============================================================
 # LED
@@ -233,7 +224,7 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to LED[4]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to LED[5]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to LED[6]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to LED[7]
-set_location_assignment PIN_AD24 -to LED[0]
+set_location_assignment PIN_AD7 -to LED[0]
 #set_location_assignment PIN_AA24 -to LED[1]
 #set_location_assignment PIN_V16 -to LED[2]
 #set_location_assignment PIN_V15 -to LED[3]
@@ -249,7 +240,7 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to SW[0]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to SW[1]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to SW[2]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to SW[3]
-set_location_assignment PIN_AB30 -to SW[0]
+set_location_assignment PIN_W25 -to SW[0]
 #set_location_assignment PIN_W24 -to SW[1]
 #set_location_assignment PIN_W21 -to SW[2]
 #set_location_assignment PIN_W20 -to SW[3]
